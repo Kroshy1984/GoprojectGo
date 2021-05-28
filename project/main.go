35de28dc43e7068227a96329093fb18a3ddad8f8
androgar <androgar@yandex.ru>
2021-05-28 15:23:38 +0300

    structure 2

--- project/main.go
+++ project/main.go
@@ -10,31 +10,25 @@ type uravnen struct {
     }
 	
 func (ur1 *uravnen) vvod() {
-    var x float64
 	fmt.Println(" Please input a="  )
-	fmt.Scanf("%v \n", &x)
-    (*ur1).a=x
+	fmt.Scanf("%v \n", &ur1.a)
 	fmt.Println(" Please input b="  )
-	fmt.Scanf("%v \n", &x)
-    (*ur1).b=x
+	fmt.Scanf("%v \n", &ur1.b)
 	fmt.Println(" Please input c="  )
-	fmt.Scanf("%v \n", &x)
-    (*ur1).c=x
+	fmt.Scanf("%v \n", &ur1.c)
 	}
 
 func (ur1 *uravnen) calcdiscr() {
-    var x float64
-	x=(*ur1).b*(*ur1).b-4*(*ur1).a*(*ur1).c
-	(*ur1).d=x
+	ur1.d=ur1.b*ur1.b-4*ur1.a*ur1.c
 	}
 
 func (ur1 *uravnen) calcroot() {
-    if (*ur1).d>0 {
-	   fmt.Println("root № 1=", (-1*(*ur1).b-math.Pow((*ur1).d,0.5))/(2*(*ur1).a))
-	   fmt.Println("root № 2=", (-1*(*ur1).b+math.Pow((*ur1).d,0.5))/(2*(*ur1).a))
-	 }else if (*ur1).d==0 {
-	   fmt.Println("root =", (-1*(*ur1).b-math.Pow((*ur1).d,0.5))/(2*(*ur1).a))
-	 }else if (*ur1).d<0 {
+    if ur1.d>0 {
+	   fmt.Println("root № 1=", (-1*ur1.b-math.Pow(ur1.d,0.5))/(2*ur1.a))
+	   fmt.Println("root № 2=", (-1*ur1.b+math.Pow(ur1.d,0.5))/(2*ur1.a))
+	 }else if ur1.d==0 {
+	   fmt.Println("root =", (-1*ur1.b-math.Pow(ur1.d,0.5))/(2*ur1.a))
+	 }else if ur1.d<0 {
 	   fmt.Println("There are not any roots ")
 	 }
 	}
